a93b9ed0d303d5ff7e6e4afb550599c696d1bcca
Akhilv7 <45240769+Akhilv7@users.noreply.github.com>
2018-12-01 11:26:49 +0530

    Add files via upload

---
+++ t.py
@@ -0,0 +1,21 @@
+Python 3.6.7 (v3.6.7:6ec5cf24b7, Oct 20 2018, 13:35:33) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\akhil\Downloads\Decision_Tree_Classification\decision_tree_classification.py 
+
+Warning (from warnings module):
+  File "C:\Users\akhil\AppData\Local\Programs\Python\Python36\lib\site-packages\sklearn\utils\validation.py", line 595
+    warnings.warn(msg, DataConversionWarning)
+DataConversionWarning: Data with input dtype int64 was converted to float64 by StandardScaler.
+
+Warning (from warnings module):
+  File "C:\Users\akhil\AppData\Local\Programs\Python\Python36\lib\site-packages\sklearn\utils\validation.py", line 595
+    warnings.warn(msg, DataConversionWarning)
+DataConversionWarning: Data with input dtype int64 was converted to float64 by StandardScaler.
+
+Warning (from warnings module):
+  File "C:\Users\akhil\AppData\Local\Programs\Python\Python36\lib\site-packages\sklearn\utils\validation.py", line 595
+    warnings.warn(msg, DataConversionWarning)
+DataConversionWarning: Data with input dtype int64 was converted to float64 by StandardScaler.
+'c' argument looks like a single numeric RGB or RGBA sequence, which should be avoided as value-mapping will have precedence in case its length matches with 'x' & 'y'.  Please use a 2-D array with a single row if you really want to specify the same RGB or RGBA value for all points.
+'c' argument looks like a single numeric RGB or RGBA sequence, which should be avoided as value-mapping will have precedence in case its length matches with 'x' & 'y'.  Please use a 2-D array with a single row if you really want to specify the same RGB or RGBA value for all points.
